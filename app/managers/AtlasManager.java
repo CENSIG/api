e98ec37505526650930b3b903e6d416723e2d466
Jean Boudet <jean.boudet@cenlr.org>
2015-04-15 18:28:29 +0200

    Add Ebean

--- app/managers/AtlasManager.java
+++ app/managers/AtlasManager.java
@@ -1,11 +1,12 @@
 package managers;
 
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.SqlRow;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import models.Type;
@@ -15,31 +16,20 @@ import play.Play;
 import play.db.DB;
 import play.libs.Json;
 
-
-public class AtlasManager extends AbstractManager
+public class AtlasManager
 {
 	private static Configuration conf = Play.application().configuration().getConfig("requetes.atlas");
 	
 	public static JsonNode info(String type)
 	{
 		Type res = new Type();
-		try {
-			PreparedStatement ps = prepared(conf.getString("info"));
-			ps.setString(1, type);
-			
-			ResultSet rs = ps.executeQuery();
-			
-			while(rs.next()) {
-				res.setObservations(rs.getInt("nbobs"));
-				res.setEspeces(rs.getInt("nbespeces"));
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		SqlRow row = Ebean.createSqlQuery(conf.getString("info"))
+				.setParameter("type", type)
+				.findUnique();
+		
+		res.setObservations(row.getInteger("nbobs"));
+		res.setEspeces(row.getInteger("nbespeces"));
 		return Json.toJson(res);
 	}
-	
-	
 
 }
