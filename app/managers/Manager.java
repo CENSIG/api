b357a1cb83164c3e1ec4591f496fd0150a362bc6
Jean Boudet <jean.boudet@cenlr.org>
2015-08-03 16:40:31 +0200

    Remove createResponse method

--- app/managers/Manager.java
+++ app/managers/Manager.java
@@ -21,55 +21,7 @@ import static play.mvc.Results.*;
  * @author Jean BOUDET
  */
 public abstract class Manager 
-{
-	
-	/**
-	 * Create a result promise with error message if error
-	 * @param 	o 		 Feedback object
-	 * @param 	message  Error message if erreur
-	 * @return	result promise
-	 */
-	protected static Promise<Result> createResponse(final Object o, final String message)
-	{
-		Promise<JsonNode> promiseObj = Promise.promise(
-			new Function0<JsonNode>() {
-				public JsonNode apply() {
-					Object objectToJson = o;
-					
-					if (o == null) {
-						objectToJson = new SimpleError(message);
-					}
-					
-					return Json.toJson(objectToJson);
-				}
-			}
-		);
-		return promiseObj.map(
-			new Function<JsonNode, Result>() {
-				public Result apply(JsonNode json) {
-					Result res = null;
-					if (json.has("error")) {
-						res = notFound(json);
-					} else {
-						res = ok(json);
-					}
-					return res;
-				}
-			}
-		);
-	}
-	
-	
-	/**
-	 * Create a result promise
-	 * @param  o Feedback object
-	 * @return result promise
-	 */
-	protected static Promise<Result> createResponse(final Object o)
-	{
-		return createResponse(o, null);
-	}
-	
+{	
 	/**
 	 * Return true if the object is ok
 	 * @param 	o	object to validate
