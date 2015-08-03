c663ff3f8031e78061da8439318a2a080dfa1113
Jean Boudet <jean.boudet@cenlr.org>
2015-08-03 16:41:04 +0200

    Update each method

--- app/managers/TaxonManager.java
+++ app/managers/TaxonManager.java
@@ -1,6 +1,5 @@
 package managers;
 
-import static utils.JsonParse.parse;
 
 import java.util.List;
 import models.ChildsModel;
@@ -13,45 +12,50 @@ import models.PhotoModel;
 import models.TaxonObsModel;
 import models.TaxonsModel;
 import play.Play;
-import play.libs.F.Promise;
-import play.mvc.Result;
+import play.libs.Json;
 
 import com.avaje.ebean.Ebean;
 
 public class TaxonManager extends Manager 
 {
 	/**
-	 * Get the response (GeoJson)
+	 * Get the data (GeoJson)
 	 * @param  id   The identifiant
-	 * @return Json response
+	 * @return data or null
 	 */
 	public static GeoJsonModel showGeoJson(Long id)
 	{
-		return Ebean.createNamedQuery(GeoJsonModel.class, "show")
+		GeoJsonModel res = Ebean.createNamedQuery(GeoJsonModel.class, "show")
 				.setParameter("id", Long.toString(id))
 				.findUnique();
 		
-		//return createResponse(parse(res), "L'identifiant "+id+" n'existe pas pour cette ressouce");
+		if (res.featuresString == null) {
+			return null;
+		}
+		
+		res.type = "FeatureCollection";
+		res.features = Json.parse(res.featuresString);
+		res.featuresString = null;
+		
+		return res;
 	}
 	
 	/**
 	 * Get the response (Informations)
 	 * @param  id   The identifiant
-	 * @return Json response
+	 * @return data or null
 	 */
 	public static InformationsModel showInformations(Long id)
 	{
-		InformationsModel res = Ebean.createNamedQuery(InformationsModel.class, "show")
+		return Ebean.createNamedQuery(InformationsModel.class, "show")
 				.setParameter("id", Long.toString(id))
 				.findUnique();
-		return res;
-		//return createResponse(res, "L'identifiant "+id+" n'existe pas pour cette ressource");
 	}
 	
 	/**
 	 * Get the response (parents)
 	 * @param 	id The identifiant
-	 * @return	Json response (json array of parents)
+	 * @return	data or null
 	 */
 	public static List<ParentsModel> showParents(Long id, String limit)
 	{
@@ -61,17 +65,15 @@ public class TaxonManager extends Manager
 				.setParameter("id", Long.toString(id))
 				.setParameter("limit", filter)
 				.findList();
-		return res;
-		//res = (isValid(res)) ? res : null;
 		
-		//return createResponse(res, "L'identifiant "+id+" n'existe pas pour cette ressource");
+		return isValid(res) ? res : null;
 	}
 	
 	/**
 	 * Get the response (childs)
 	 * @param id the identifiant
 	 * @param search filter for child name
-	 * @return Json response (json array of childs)
+	 * @return data or null
 	 */
 	public static List<ChildsModel> showChilds(Long id, String search)
 	{
@@ -79,26 +81,21 @@ public class TaxonManager extends Manager
 				.setParameter("id", Long.toString(id))
 				.setParameter("q", search+"%")
 				.findList();
-		return res;
-		//res = (isValid(res)) ? res : null;
 		
-		//return createResponse(res, "Aucun taxon trouvé pour la ressource "+id+" et le filtre "+search);
+		return isValid(res) ? res : null;
 	}
 	
 	/**
 	 * Get the response (brothers)
 	 * @param id the cdnom
-	 * @return Json response (json array of brothers)
+	 * @return data or null
 	 */
 	public static List<TaxonsModel> showBrothers(Long id)
 	{
 		List<TaxonsModel> res = Ebean.createNamedQuery(TaxonsModel.class, "show")
 				.setParameter("id", Long.toString(id))
 				.findList();
-		return res;
-		//res = (isValid(res)) ? res : null;
-		
-		//return createResponse(res, "Aucun frêres trouvés pour la ressource "+id);
+		return isValid(res) ? res : null;
 	}
 	
 	/**
@@ -106,64 +103,51 @@ public class TaxonManager extends Manager
 	 * @param id	 The cdnom
 	 * @param ordre	 The ordre
 	 * @param format (optionnal) output for json
-	 * @return Json response (json array of first childs)
+	 * @return data or null
 	 */
 	public static List<TaxonObsModel> showFirstChildObs(Long id)
 	{
 		List<TaxonObsModel> res = Ebean.createNamedQuery(TaxonObsModel.class, "firstChildObs")
 				.setParameter("id", Long.toString(id))
 				.findList();
-		return res;
-		//res = (isValid(res)) ? res : null;
-		
-		//return createResponse(res, "Aucun fils direct trouvés pour la ressource "+id);
+		return isValid(res) ? res : null;
 	}
 
 	/**
 	 * Get the response (photo)
 	 * @param id the cdnom
-	 * @return Json response (json array photo)
+	 * @return data or null
 	 */
 	public static List<PhotoModel> showPhotos(Long id) {
 		
 		List<PhotoModel> res = Ebean.createNamedQuery(PhotoModel.class, "show")
 				.setParameter("id", Long.toString(id))
 				.findList();
-		return res;
-		//res = (isValid(res)) ? res : null;
-		
-		//return createResponse(res, "Aucune photos trouvés pour la ressource "+id);
+		return isValid(res) ? res : null;
 	}
 
 	/**
 	 * Get the response (monographie)
 	 * @param id the cdnom
-	 * @return Json response (json list monographies)
+	 * @return data or null
 	 */
 	public static List<MonographieModel> showMonographies(Long id) {
 		List<MonographieModel> res = Ebean.createNamedQuery(MonographieModel.class, "show")
 				.setParameter("id", Long.toString(id))
 				.findList();
-		return res;
-		//res = (isValid(res)) ? res : null;
-		
-		//return createResponse(res, "Aucune monographies trouvés pour la ressource "+id);
+		return isValid(res) ? res : null;
 	}
 
 	/**
 	 * Get the response (phenologie)
 	 * @param id the cdnom
-	 * @return	Json response (json phenologie: mois, total, adultes, larves)
+	 * @return	data or null
 	 */
 	public static List<PhenologieModel> showPhenologie(Long id) {
 		List<PhenologieModel> res = Ebean.createNamedQuery(PhenologieModel.class, "show")
 				.setParameter("id", Long.toString(id))
 				.findList();
-		return res;
-		
-		//res = (isValid(res)) ? res : null;
-		
-		//return createResponse(res, "Aucune observations pour la ressource "+id);
+		return isValid(res) ? res : null;
 	}
 }
 
