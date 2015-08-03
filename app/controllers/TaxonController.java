3d287c96eb3d9936f72e63e94e7d5b5db49c18f2
Jean Boudet <jean.boudet@cenlr.org>
2015-08-03 16:40:01 +0200

    Update async action controllers

--- app/controllers/TaxonController.java
+++ app/controllers/TaxonController.java
@@ -16,17 +16,15 @@ import models.PhotoModel;
 import models.TaxonObsModel;
 import models.TaxonsModel;
 import play.libs.Akka;
-import play.libs.F;
 import play.libs.F.Function;
 import play.libs.F.Function0;
 import play.libs.F.Promise;
 import play.libs.HttpExecution;
-import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.With;
 import scala.concurrent.ExecutionContext;
-import static utils.JsonParse.parse;
+import static utils.BuildResult.build;
 
 /**
  * 
@@ -37,96 +35,74 @@ import static utils.JsonParse.parse;
 @With(CorsAction.class)
 public class TaxonController extends Controller 
 {
-	//private static ExecutionContext context = Akka.system().dispatchers().lookup("play.akka.actor.my-context");
+	private static ExecutionContext bigRequest = HttpExecution.fromThread(Akka.system().dispatchers().lookup("play.akka.actor.bigRequest"));
 	
 	/**
 	 * Show GeoJson for a ressource with specific id
 	 * @param id
-	 * @return The GeoJson
+	 * @return The GeoJson result
 	 */
 	public static Promise<Result> showGeoJson(final Long id)
 	{
-		ExecutionContext context = HttpExecution.fromThread(Akka.system().dispatchers().lookup("play.akka.actor.my-context"));
-		
-		F.Promise<GeoJsonModel> promise = F.Promise.promise(
-				new Function0<GeoJsonModel>() {
-					public GeoJsonModel apply() {
-						GeoJsonModel geojson = TaxonManager.showGeoJson(id);
-						geojson.features = Json.parse(geojson.featuresString);
-						geojson.featuresString = null;
-						return geojson;
-					}
-				}, context
-		);
-		
-		return promise.map(
+		return Promise.promise(
+			new Function0<GeoJsonModel>() {
+				public GeoJsonModel apply() {
+					return TaxonManager.showGeoJson(id);
+				}
+			}, bigRequest
+		).map(
 			new Function<GeoJsonModel, Result>() {
 				public Result apply(GeoJsonModel geojson) {
-					return ok(Json.toJson(geojson));
+					return build(geojson, "Aucune observations pour la ressource "+id);
 				}
-			}, context
+			}, bigRequest
 		);
-		
-		//return geojson == null ? notFound("pas trouvé") : ok(Json.toJson(geojson));
 	}
 	
 	/**
 	 * Show informations for a ressource with specific id
 	 * @param id
-	 * @return The informations
+	 * @return The informations result
 	 */
 	public static Promise<Result> showInformations(final Long id)
-	{
-		//ExecutionContext context = Akka.system().dispatchers().lookup("play.akka.actor.my-context");
-		ExecutionContext context = HttpExecution.fromThread(Akka.system().dispatchers().lookup("play.akka.actor.my-context"));
-		
-		F.Promise<InformationsModel> promise = F.Promise.promise(
-				new Function0<InformationsModel>() {
-					public InformationsModel apply() {
-						InformationsModel info = TaxonManager.showInformations(id);
-						return info;
-					}
-				}, context
-		);
-		
-		return promise.map(
+	{	
+		return Promise.promise(
+			new Function0<InformationsModel>() {
+				public InformationsModel apply() {
+					return TaxonManager.showInformations(id);
+				}
+			}, bigRequest
+		).map(
 			new Function<InformationsModel, Result>() {
 				public Result apply(InformationsModel info) {
-					return ok(Json.toJson(info));
+					return build(info, "Aucune informations trouvées pour la ressource "+id);
 				}
-			}, context
+			}, bigRequest
 		);
-		//return TaxonManager.showInformations(id);
 	}
 	
 	/**
 	 * Show parents for a specific taxon
 	 * @param  id
 	 * @param  limit limit for display hierarchie (default KD)
-	 * @return The parents
+	 * @return The parents result
 	 */
 	@StringParamAnnotation("limit")
 	public static Promise<Result> showParents(final Long id, final String limit)
 	{
-		//ExecutionContext context = Akka.system().dispatchers().lookup("play.akka.actor.my-context");
-		
-		F.Promise<List<ParentsModel>> promise = F.Promise.promise(
-				new Function0<List<ParentsModel>>() {
-					public List<ParentsModel> apply() {
-						List<ParentsModel> info = TaxonManager.showParents(id, limit);
-						return info;
-					}
+		return Promise.promise(
+			new Function0<List<ParentsModel>>() {
+				public List<ParentsModel> apply() {
+					return TaxonManager.showParents(id, limit);
 				}
-		);
-		
-		return promise.map(
+			}
+		).map(
 			new Function<List<ParentsModel>, Result>() {
-				public Result apply(List<ParentsModel> info) {
-					return ok(Json.toJson(info));
+				public Result apply(List<ParentsModel> parents) {
+					return build(parents, "Aucun parents trouvés pour la ressource "+id);
 				}
 			}
 		);
-		//return TaxonManager.showParents(id, limit);
 	}
 	
 	/**
@@ -139,25 +115,19 @@ public class TaxonController extends Controller
 	@StringParamAnnotation("q")
 	public static Promise<Result> showChilds(final Long id, final String q)
 	{
-		//ExecutionContext context = Akka.system().dispatchers().lookup("play.akka.actor.my-context");
-		
-		F.Promise<List<ChildsModel>> promise = F.Promise.promise(
-				new Function0<List<ChildsModel>>() {
-					public List<ChildsModel> apply() {
-						List<ChildsModel> info = TaxonManager.showChilds(id, q);
-						return info;
-					}
+		return Promise.promise(
+			new Function0<List<ChildsModel>>() {
+				public List<ChildsModel> apply() {
+					return TaxonManager.showChilds(id, q);
 				}
-		);
-		
-		return promise.map(
+			}
+		).map(
 			new Function<List<ChildsModel>, Result>() {
-				public Result apply(List<ChildsModel> info) {
-					return ok(Json.toJson(info));
+				public Result apply(List<ChildsModel> childs) {
+					return build(childs, "Aucun enfants trouvés pour la ressource "+id+" avec le filtre "+q);
 				}
 			}
 		);
-		//return TaxonManager.showChilds(id, q);
 	}
 	
 	/**
@@ -166,28 +136,21 @@ public class TaxonController extends Controller
 	 * @return the brothers
 	 */
 	public static Promise<Result> showBrothers(final Long id)
-	{
-		//ExecutionContext context = Akka.system().dispatchers().lookup("play.akka.actor.my-context");
-		
-		F.Promise<List<TaxonsModel>> promise = F.Promise.promise(
+	{	
+		return Promise.promise(
 				new Function0<List<TaxonsModel>>() {
 					public List<TaxonsModel> apply() {
-						List<TaxonsModel> info = TaxonManager.showBrothers(id);
-						return info;
+						return TaxonManager.showBrothers(id);
 					}
 				}
-		);
-		
-		return promise.map(
+		).map(
 			new Function<List<TaxonsModel>, Result>() {
-				public Result apply(List<TaxonsModel> info) {
-					return ok(Json.toJson(info));
+				public Result apply(List<TaxonsModel> brothers) {
+					return build(brothers, "Aucun frères trouvés pour la ressource "+id);
 				}
 			}
 		);
-		//return TaxonManager.showBrothers(id);
 	}
-	
 	
 	/**
 	 * Show first child (with obs) of a specific taxon
@@ -197,27 +160,21 @@ public class TaxonController extends Controller
 	 * @return The first child
 	 */
 	public static Promise<Result> showFirstChildObs(final Long id)
-	{
-		//ExecutionContext context = Akka.system().dispatchers().lookup("play.akka.actor.my-context");
-		ExecutionContext context = HttpExecution.fromThread(Akka.system().dispatchers().lookup("play.akka.actor.my-context"));
-		
-		F.Promise<List<TaxonObsModel>> promise = F.Promise.promise(
-				new Function0<List<TaxonObsModel>>() {
-					public List<TaxonObsModel> apply() {
-						List<TaxonObsModel> info = TaxonManager.showFirstChildObs(id);
-						return info;
-					}
-				}, context
-		);
-		
-		return promise.map(
-			new Function<List<TaxonObsModel>, Result>() {
-				public Result apply(List<TaxonObsModel> info) {
-					return ok(Json.toJson(info));
+	{	
+		return Promise.promise(
+			new Function0<List<TaxonObsModel>>() {
+				public List<TaxonObsModel> apply() {
+					List<TaxonObsModel> info = TaxonManager.showFirstChildObs(id);
+					return info;
 				}
-			}, context
+			}, bigRequest
+		).map(
+			new Function<List<TaxonObsModel>, Result>() {
+				public Result apply(List<TaxonObsModel> obs) {
+					return build(obs, "Aucune observations trouvées pour les enfants direct de la ressource "+id);
+				}
+			}, bigRequest
 		);
-		//return TaxonManager.showFirstChildObs(id);
 	}
 	
 	/**
@@ -227,25 +184,19 @@ public class TaxonController extends Controller
 	 */
 	public static Promise<Result> showPhotos(final Long id)
 	{
-		//ExecutionContext context = Akka.system().dispatchers().lookup("play.akka.actor.my-context");
-		
-		F.Promise<List<PhotoModel>> promise = F.Promise.promise(
-				new Function0<List<PhotoModel>>() {
-					public List<PhotoModel> apply() {
-						List<PhotoModel> info = TaxonManager.showPhotos(id);
-						return info;
-					}
+		return Promise.promise(
+			new Function0<List<PhotoModel>>() {
+				public List<PhotoModel> apply() {
+					return TaxonManager.showPhotos(id);
 				}
-		);
-		
-		return promise.map(
+			}
+		).map(
 			new Function<List<PhotoModel>, Result>() {
-				public Result apply(List<PhotoModel> info) {
-					return ok(Json.toJson(info));
+				public Result apply(List<PhotoModel> photos) {
+					return build(photos, "Aucune photos trouvées pour la ressource "+id);
 				}
 			}
 		);
-		//return TaxonManager.showPhotos(id);
 	}
 	
 	/**
@@ -255,25 +206,19 @@ public class TaxonController extends Controller
 	 */
 	public static Promise<Result> showMonographies(final Long id)
 	{
-		//ExecutionContext context = Akka.system().dispatchers().lookup("play.akka.actor.my-context");
-		
-		F.Promise<List<MonographieModel>> promise = F.Promise.promise(
-				new Function0<List<MonographieModel>>() {
-					public List<MonographieModel> apply() {
-						List<MonographieModel> info = TaxonManager.showMonographies(id);
-						return info;
-					}
+		return Promise.promise(
+			new Function0<List<MonographieModel>>() {
+				public List<MonographieModel> apply() {
+					return TaxonManager.showMonographies(id);
 				}
-		);
-		
-		return promise.map(
+			}
+		).map(
 			new Function<List<MonographieModel>, Result>() {
-				public Result apply(List<MonographieModel> info) {
-					return ok(Json.toJson(info));
+				public Result apply(List<MonographieModel> monographies) {
+					return build(monographies, "Aucune monographies trouvées pour la ressource "+id);
 				}
 			}
 		);
-		//return TaxonManager.showMonographies(id);
 	}
 	
 	/**
@@ -282,24 +227,19 @@ public class TaxonController extends Controller
 	 * @return the phenologie
 	 */
 	public static Promise<Result> showPhenologie(final Long id)
-	{
-		//ExecutionContext context = Akka.system().dispatchers().lookup("play.akka.actor.my-other-context");
-		
-		F.Promise<List<PhenologieModel>> promise = F.Promise.promise(
-				new Function0<List<PhenologieModel>>() {
-					public List<PhenologieModel> apply() {
-						List<PhenologieModel> list = TaxonManager.showPhenologie(id);
-						return list;
-					}
+	{	
+		return Promise.promise(
+			new Function0<List<PhenologieModel>>() {
+				public List<PhenologieModel> apply() {
+					return TaxonManager.showPhenologie(id);
 				}
-		);
-		
-		return promise.map(
+			}, bigRequest
+		).map(
 			new Function<List<PhenologieModel>, Result>() {
-				public Result apply(List<PhenologieModel> list) {
-					return ok(Json.toJson(list));
+				public Result apply(List<PhenologieModel> phenologie) {
+					return build(phenologie, "Aucune observations pour la ressource "+id);
 				}
-			}
+			}, bigRequest
 		);
 	}
 }
