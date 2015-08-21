62822c4559e3e89c5b7d6d5bdf1127df8cbfca4f
Jean Boudet <jean.boudet@cenlr.org>
2015-08-21 09:19:47 +0200

    Check body post request

---
+++ app/actions/BodyIsJson.java
@@ -0,0 +1,34 @@
+package actions;
+
+import errors.SimpleError;
+import play.libs.F.Promise;
+import play.libs.Json;
+import play.mvc.Action.Simple;
+import play.mvc.Http.Context;
+import play.mvc.Result;
+
+/**
+ * For post request. The body must be in json
+ * @author Jean BOUDET 
+ */
+public class BodyIsJson extends Simple 
+{
+	
+	private final static String CONTENT_TYPE = "application/json";
+
+	@Override
+	public Promise<Result> call(Context ctx) throws Throwable 
+	{
+		Promise<Result> res = delegate.call(ctx);
+		String[] type = ctx.request().getHeader("Content-Type").split(";");
+		
+		if (!type[0].equals(CONTENT_TYPE)) {
+			res = Promise.pure((Result) badRequest(
+				Json.toJson(new SimpleError("Impossible de traiter la requête"))
+			));
+		}
+		
+		return res;
+	}
+
+}
