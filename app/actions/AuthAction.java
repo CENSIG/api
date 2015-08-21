bf04f33f1653fbe3ad44065623e4d3a310eb84af
Jean Boudet <jean.boudet@cenlr.org>
2015-08-21 09:15:49 +0200

    Update authentification

--- app/actions/AuthAction.java
+++ app/actions/AuthAction.java
@@ -1,18 +1,33 @@
 package actions;
 
-import play.Logger;
+import errors.SimpleError;
+import play.cache.Cache;
 import play.libs.F.Promise;
+import play.libs.Json;
 import play.mvc.Action.Simple;
 import play.mvc.Http.Context;
 import play.mvc.Result;
 
-public class PrivateAction extends Simple {
+/**
+ * Action for check token and client id
+ * @author Jean BOUDET
+ */
+public class AuthAction extends Simple {
 
 	@Override
 	public Promise<Result> call(Context ctx) throws Throwable {
-		Logger.debug(ctx.request().remoteAddress());
-		Promise<Result> res = delegate.call(ctx);
+		Promise<Result> res = Promise.pure((Result) unauthorized(
+				Json.toJson(new SimpleError("Impossible d'accèder à cette ressource"))
+		));
 		
+		String token = ctx.request().getHeader("X-Access-Token");
+		String clientId = ctx.request().getHeader("X-Client-Id");
+		
+		Object tokenClientIdCache = Cache.get(clientId);
+		
+		if ((clientId != null && token != null) && (tokenClientIdCache != null)) {
+			res = delegate.call(ctx);
+		}
 		
 		return res;
 	}
