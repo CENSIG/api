e03e424fdbbbcd4030ebe51528e0f9240fdf2dbf
Jean Boudet <jean.boudet@cenlr.org>
2015-08-21 09:15:18 +0200

    Add onRequest handler

--- app/Global.java
+++ app/Global.java
@@ -1,17 +1,30 @@
+import java.lang.reflect.Method;
+
 import errors.SimpleError;
+import actions.CorsActionWrapper;
 import play.GlobalSettings;
 import play.libs.F.Promise;
 import play.libs.Json;
+import play.mvc.Action;
+import play.mvc.Http.Request;
 import play.mvc.Http.RequestHeader;
 import play.mvc.Result;
-import static play.mvc.Results.*;
+import static play.mvc.Results.notFound;
+import static play.mvc.Results.badRequest;
 
 /**
  * Global settings for handle errors
  * @author Jean BOUDET
  */
 public class Global extends GlobalSettings 
-{
+{	
+	/*(non-Javadoc)
+	 * @see play.GlobalSettings#onRequest(play.mvc.Http.Request, java.lang.reflect.Method)
+	 */
+	public Action<?> onRequest(Request req, Method actionMethod)
+	{
+		return new CorsActionWrapper(super.onRequest(req, actionMethod));
+	}
 	
 	/* (non-Javadoc)
 	 * @see play.GlobalSettings#onHandlerNotFound(play.mvc.Http.RequestHeader)
