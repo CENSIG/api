37f4a92c8fde951fd73773264fac6693548fe50e
Jean Boudet <jean.boudet@cenlr.org>
2015-08-28 08:46:18 +0200

    Add user model

---
+++ app/models/UserModel.java
@@ -0,0 +1,21 @@
+package models;
+
+import javax.persistence.Entity;
+
+import com.avaje.ebean.annotation.Sql;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import play.db.ebean.Model;
+
+@Entity
+@Sql
+@JsonInclude(Include.NON_NULL)
+public class UserModel extends Model 
+{
+	public String firstNameLetter;
+	
+	public String name;
+	
+	public String firstName;
+}
